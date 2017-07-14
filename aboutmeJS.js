e169a3b811794c4cf94148023771fbf6afc0c686
fmabees <fmabees@live.com>
2017-07-14 08:46:48 -0700

    Update aboutmeJS.js

--- aboutmeJS.js
+++ aboutmeJS.js
@@ -1,14 +1,88 @@
-var cat = document.getElementById("cat");
-var arrow1 = document.getElementById("arrow1");
-function hover(object) {
-  object.style.width = "400px";
+body {
+    	font-family: "Kalam";
+			font-size: 20px;
 }
-function smaller(object) {
-  object.style.width = "350px";
+
+h1 {
+			font-family: Cursive;
 }
-function background() {
-  document.body.style.backgroundImage = "url('background.jpg')";
+th {
+			border: 1px solid black;
+			width: 50%;
 }
-function scrollWin(x) {
-  window.scrollTo(0,x);
+table, td {
+			border: 1px solid black;
+}
+html {
+  margin-left: 50px;
+  margin-right: 50px;
+  background-color: Plum;
+  background: url("background.jpg") no-repeat center center fixed;
+  background-size: cover;
+
+}
+#cat {
+  width:350px;
+  margin-bottom:10px;
+  border: 5px solid White;
+  margin-top: 50px;
+  margin-right: 30px;
+}
+#biog {
+  margin-top: 50px;
+}
+#funFacts {
+  margin-top: 100px;
+}
+#arrow1 {
+  width:100px;
+}
+#quote {
+  font-family: "Permanent Marker";
+  font-size: 80px;
+  color: PaleVioletRed;
+  line-height: 200px;
+}
+#ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: rgba(0, 0, 0, 0.1);
+    position: fixed;
+    top: 0;
+    width: 93%;
+}
+
+#li {
+    float: left;
+}
+
+#li #a {
+    display: block;
+    color: White;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-size: 25px;
+}
+
+#li #a:hover {
+    background-color: White;
+    color: Black;
+}
+
+#a.active .topNav {
+    background-color: White;
+    color: Black;
+}
+
+#myContainer {
+  width: 100%;
+  height: 100px;
+  position: relative;
+  background: transparent;
+}
+#myAnimation {
+  position: absolute;
 }
